a5a638aa803db625207560d5155b3067d2038976
luminadesignerpro <luminadesignerpro@gmail.com>
2026-09-02 14:19:56 -0300

    Release SD Construcoes Ltda - Sistema de Obras e Terceirizados

---
+++ Abrir_SDConstrucao.bat
@@ -0,0 +1,8 @@
+@echo off
+title SD Construcao PRO - Sistema de Gestao de Obras
+echo ==========================================
+echo    INICIANDO SD CONSTRUCAO PRO
+echo ==========================================
+echo Abrindo o sistema no navegador...
+start "" "%~dp0index.html"
+exit
